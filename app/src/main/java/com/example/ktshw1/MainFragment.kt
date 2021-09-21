946c15386a9bceafd74e82f46d8ab423cb7b06cc
Aleksandr Tagilov <saniatagilov@gmail.com>
2021-09-22 01:27:58 +0300

    add recycler items (rebase)

--- app/src/main/java/com/example/ktshw1/MainFragment.kt
+++ app/src/main/java/com/example/ktshw1/MainFragment.kt
@@ -1,6 +1,9 @@
 package com.example.ktshw1
 
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.ktshw1.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
+    private val binding: FragmentMainBinding by viewBinding (FragmentMainBinding::bind)
 }
