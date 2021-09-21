1bb1f16b63e3f733f8876bcd2e6273239e143461
Aleksandr Tagilov <saniatagilov@gmail.com>
2021-09-21 22:24:36 +0300

    minor fixes

--- app/src/main/java/com/example/ktshw1/MainActivity.kt
+++ app/src/main/java/com/example/ktshw1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.ktshw1
 
 import android.R.attr.button
+import android.annotation.SuppressLint
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.drawable.Drawable
@@ -10,41 +11,49 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.annotation.ColorInt
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
-    var color: Int = R.color.black
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    @ColorInt
+    private var color: Int? = null
+    private var hand: ImageView? = null
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val hand = findViewById<ImageView>(R.id.hand)
+        color = resources.getColor(R.color.black, theme)
+        hand = findViewById(R.id.hand)
         val btn = findViewById<Button>(R.id.change_color)
         val rnd = Random()
-        color = resources.getColor(R.color.black, theme)
         btn?.setOnClickListener {
-            setImageColor(hand, Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)))
+            setImageColor(hand!!, Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)))
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun setImageColor(v: ImageView, c: Int) {
         v.imageTintList = ColorStateList.valueOf(c)
         color = c
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putInt("color", color)
+        outState.putInt(COLOR_KEY, color!!)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        val hand = findViewById<ImageView>(R.id.hand)
-        color = savedInstanceState.getInt("color")
-        setImageColor(hand, color)
+        color = savedInstanceState.getInt(COLOR_KEY)
+        setImageColor(hand!!, color!!)
+    }
+
+    companion object {
+        private val COLOR_KEY = "color"
     }
 }
