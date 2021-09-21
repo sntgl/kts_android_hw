7fb78e895c628f321bb056e13a183afe58c3f635
Aleksandr Tagilov <saniatagilov@gmail.com>
2021-09-22 00:09:17 +0300

    move to viewBinding (rebase)

--- app/src/main/java/com/example/ktshw1/LoginFragment.kt
+++ app/src/main/java/com/example/ktshw1/LoginFragment.kt
@@ -4,40 +4,31 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
+import by.kirich1409.viewbindingdelegate.viewBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
-    private var pass: TextInputEditText? = null
-    private var email: TextInputEditText? = null
-    private var emailLayout: TextInputLayout? = null
-    private var passLayout: TextInputLayout? = null
-    private var loginBtn: Button? = null
+    private var passValid = false
+    private var emailValid = false
     val model: AppViewModel by viewModels()
+    private val binding: FragmentLoginBinding by viewBinding(FragmentLoginBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        emailLayout = view.findViewById(R.id.email_field_layout)
-        passLayout = view.findViewById(R.id.password_field_layout)
-        email = view.findViewById(R.id.email_field)
-        pass = view.findViewById(R.id.password_field)
-        loginBtn = view.findViewById(R.id.login_button)
         bindFields()
     }
 
     private fun bindFields() {
-        pass?.addTextChangedListener(object : TextWatcher {
+        binding.passwordField.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) {
                 model.onEditPass(s.toString())
             }
         })
-        email?.addTextChangedListener(object : TextWatcher {
+        binding.emailField.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) {
@@ -60,25 +51,16 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             else
                 loginBtn?.alpha = 0.5F
         })
-        loginBtn?.setOnClickListener {
+        binding.loginButton.setOnClickListener {
             if (model.loginState.value == true)
                 findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
         }
         //Автоматическое нажатие на кнопки при валидном вводе+нажатии Enter
-        pass?.setOnEditorActionListener { _, _, _ ->
+        binding.passwordField.setOnEditorActionListener { _, _, _ ->
             if (model.loginState.value == true) {
                 loginBtn?.performClick()
             }
             return@setOnEditorActionListener (model.loginState.value != true)
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        pass = null
-        email = null
-        passLayout = null
-        emailLayout = null
-        loginBtn = null
     }
 }
--- app/src/main/java/com/example/ktshw1/OnBoardingFragment.kt
+++ app/src/main/java/com/example/ktshw1/OnBoardingFragment.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.ktshw1.databinding.FragmentOnBoardingBinding
 
 class OnBoardingFragment : Fragment(R.layout.fragment_on_boarding) {
 
+    private val binding: FragmentOnBoardingBinding by viewBinding(FragmentOnBoardingBinding::bind)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btn = requireView().findViewById<Button>(R.id.start_button)
-        btn.setOnClickListener {
+        binding.startButton.setOnClickListener {
             findNavController().navigate(R.id.action_onBoardingFragment_to_loginFragment)
         }
-
     }
 }
