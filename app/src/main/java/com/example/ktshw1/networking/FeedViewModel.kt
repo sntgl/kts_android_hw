ee70131ec1c4816ebd359ff6485531675690dcd5
Aleksandr Tagilov <saniatagilov@gmail.com>
2021-10-08 00:26:58 +0300

    added pagination

--- app/src/main/java/com/example/ktshw1/networking/FeedViewModel.kt
+++ app/src/main/java/com/example/ktshw1/networking/FeedViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.ktshw1.model.FeedLoading
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import studio.kts.android.school.lection4.networking.data.FeedRepository
@@ -12,12 +13,12 @@ import timber.log.Timber
 class FeedViewModel: ViewModel() {
     private val repository = FeedRepository()
 
-    private val feedLiveData = MutableLiveData<List<Subreddit>>(emptyList())
+    private val feedLiveData = MutableLiveData<List<*>>(listOf(FeedLoading()))
     private val isLoadingLiveData = MutableLiveData(false)
 
     private var currentSearchJob: Job? = null
 
-    val feedList: LiveData<List<Subreddit>>
+    val feedList: LiveData<List<*>>
         get() = feedLiveData
 
     val isLoading: LiveData<Boolean>
@@ -31,15 +32,16 @@ class FeedViewModel: ViewModel() {
             runCatching {
                 Timber.tag("LOAD").d("Request sent")
                 repository.getBestFeed()
-            }.onSuccess { value ->
+            }.onSuccess {
                 Timber.tag("LOAD").d("Success")
                 isLoadingLiveData.postValue(false)
-                feedLiveData.postValue(value)
+                postLoading(it)
+//                feedLiveData.postValue(value+FeedLoading())
             }.onFailure {
                 Timber.tag("LOAD").d("Error")
                 timber.e(it)
                 isLoadingLiveData.postValue(false)
-                feedLiveData.postValue(emptyList())
+                postLoading(null)
             }
         }
     }
@@ -48,20 +50,30 @@ class FeedViewModel: ViewModel() {
         if (isLoading.value == true) return
         isLoadingLiveData.postValue(true)
         currentSearchJob?.cancel()
+        val after: String = getLastSubreddit()
         currentSearchJob = viewModelScope.launch {
             runCatching {
-                Timber.tag("LOAD next").d("Request sent")
-                repository.getNextBestFeed(feedLiveData.value?.last()?.id ?: "")
+                Timber.tag("LOAD next").d("Request sent, next is $after")
+                repository.getNextBestFeed(after)
             }.onSuccess {
                 Timber.tag("LOAD next").d("Success")
                 isLoadingLiveData.postValue(false)
-                feedLiveData.postValue(feedLiveData.value?.plus(it))
+                postLoading(feedLiveData.value?.plus(it))
             }.onFailure {
                 Timber.tag("LOAD next").d("Error")
                 Timber.tag("LOAD next").e(it)
                 isLoadingLiveData.postValue(false)
-                feedLiveData.postValue(emptyList())
             }
         }
+    }
+
+    private fun getLastSubreddit(): String {
+        return (feedLiveData.value?.dropLast(1)?.last() as Subreddit).id
+    }
+
+    private fun postLoading(l: List<*>?) {
+        var dropLast = feedLiveData.value?.dropLast(1) ?: emptyList()
+        dropLast = dropLast.plus(l?.plus(FeedLoading()) ?: listOf(FeedLoading()))
+        feedLiveData.postValue(dropLast)
     }
 }
